07e80e76bcf7dc79c571a00776647311f4d735d0
Egil Valdemar Dam Wendsjø <egil.wendsjo@gmail.com>
2022-04-22 10:54:06 +0200

    DateViewModel added

---
+++ Ugeplan_System/Ugeplan_System/ViewModel/DateViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ugeplan_System.ViewModel
+{
+    public class DateViewModel
+    {
+        public string Day { get; set; }
+        public DateTime ScheduleDate { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+
+        public DateViewModel()
+        {
+
+        }
+
+        public DateViewModel(string day, DateTime scheduleDate, string startTime, string endTime)
+        {
+            Day = day;
+            ScheduleDate = scheduleDate;
+            StartTime = startTime;
+            EndTime = endTime;
+        }
+
+        public override string ToString()
+        {
+            return $"{Day}, {ScheduleDate} - {StartTime} : {EndTime}";
+        }
+    }
+}
