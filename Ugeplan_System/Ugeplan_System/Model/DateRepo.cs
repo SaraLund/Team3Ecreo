04d9573409c4b6e9340cf81ad9db8d575ce145e1
SaraLund <masaia121@gmail.com>
2022-04-22 10:36:28 +0200

    Added methods in DateRepo

--- Ugeplan_System/Ugeplan_System/Model/DateRepo.cs
+++ Ugeplan_System/Ugeplan_System/Model/DateRepo.cs
@@ -19,7 +19,29 @@ namespace Ugeplan_System.Model
 
         public void InitializeRepo()
         {
+            Date date;
 
+            using(SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+
+                SqlCommand command = new SqlCommand("SELECT DateId, Day, StartTime, EndTime FROM DateTable", conn);
+
+                using(SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        date = new Date();
+                        date.DateId = int.Parse(reader["DateId"].ToString());
+                        date.Day = reader["Day"].ToString();
+                        date.ScheduleDate = DateTime.Parse(reader["Date"].ToString());
+                        date.StartTime = reader["StartTime"].ToString();
+                        date.EndTime = reader["EndTime"].ToString();
+
+                        dates.Add(date);
+                    }
+                }
+            }
         }
 
         public void AddDate(DateTime date, string start, string end)
@@ -48,15 +70,47 @@ namespace Ugeplan_System.Model
 
         public void RemoveDate(Date oldDate)
         {
+            if (dates.Contains(oldDate))
+            {
+                dates.Remove(oldDate);
+                
+                using(SqlConnection conn = new SqlConnection(connStr))
+                {
+                    conn.Open();
 
+                    SqlCommand command = new SqlCommand("DELETE FROM DateTable WHERE DateId = @dateId;", conn);
+
+                    command.Parameters.Add("@dateId", System.Data.SqlDbType.Int).Value = oldDate.DateId;
+
+                    command.ExecuteNonQuery();
+                }
+            }
         }
 
-        public void UpdateDate(DateTime newStart, DateTime newEnd)
+        public void UpdateDate(int dateId, string newStart, string newEnd)
         {
+            if(dates.Exists(d => d.DateId == dateId))
+            {
+                Date temp = dates.Find(d => d.DateId == dateId);
+                temp.StartTime = newStart;
+                temp.EndTime = newEnd;
 
+                using(SqlConnection conn = new SqlConnection(connStr))
+                {
+                    conn.Open();
+
+                    SqlCommand command = new SqlCommand("UPDATE DateTable SET StartTime = @newStart, EndTime = @newEnd" +
+                                                        "WHERE DateId = @dateId", conn);
+                    command.Parameters.Add("@newStart", System.Data.SqlDbType.NVarChar).Value = newStart;
+                    command.Parameters.Add("@newEnd", System.Data.SqlDbType.NVarChar).Value = newEnd;
+                    command.Parameters.Add("@dateId", System.Data.SqlDbType.Int).Value = dateId;
+
+                    command.ExecuteNonQuery();
+                }
+            }
         }
 
-        public List<Date> GetDates()
+        public List<Date> GetAllDates()
         {
             return dates;
         }
