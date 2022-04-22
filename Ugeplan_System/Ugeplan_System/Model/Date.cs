f06ff042691432a2a05220ae4a7d405f3bd58494
Plutos-Fungus <rasmus1070@gmail.com>
2022-04-22 10:59:10 +0200

    Merged a couple of files from other branches into this branch

--- Ugeplan_System/Ugeplan_System/Model/Date.cs
+++ Ugeplan_System/Ugeplan_System/Model/Date.cs
@@ -4,21 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Ugeplan_System.ViewModel
+namespace Ugeplan_System.Model
 {
-    public class DateViewModel
+    public class Date
     {
+        public int DateId { get; set; }
         public string Day { get; set; }
         public DateTime ScheduleDate { get; set; }
         public string StartTime { get; set; }
         public string EndTime { get; set; }
 
-        public DateViewModel()
+        public Date()
         {
 
         }
 
-        public DateViewModel(string day, DateTime scheduleDate, string startTime, string endTime)
+        public Date(string day, DateTime scheduleDate, string startTime, string endTime)
         {
             Day = day;
             ScheduleDate = scheduleDate;
