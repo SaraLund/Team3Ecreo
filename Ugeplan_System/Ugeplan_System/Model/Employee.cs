3c9113a8777fdbcb0ac006334d530a337055031d
Casper Flindt Sidor <cfsi37873@edu.ucl.dk>
2022-04-22 12:48:45 +0200

    EmployeeRepo added

--- Ugeplan_System/Ugeplan_System/Model/Employee.cs
+++ Ugeplan_System/Ugeplan_System/Model/Employee.cs
@@ -8,18 +8,24 @@ namespace Ugeplan_System.Model
 {
     public class Employee
     {
-        
+        public int EmployeeId { get; set; }
         public string Name { get; set; }
         public string JobPosition { get; set; }
         public string Mail { get; set; }
         public string PhoneNumber { get; set; }
 
-        public Employee(string name, string jobPosition, string mail, string phoneNumber)
+        public Employee(int employeeId, string name, string jobPosition, string mail, string phoneNumber)
         {
+            EmployeeId = employeeId;
             Name = name;
             JobPosition = jobPosition;
             Mail = mail;
             PhoneNumber = phoneNumber;
+        }
+
+        public Employee()
+        {
+
         }
 
         public override string ToString()
---
+++ Ugeplan_System/Ugeplan_System/Model/EmployeeRepo.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ugeplan_System.Model
+{
+    public class EmployeeRepo
+    {
+        List<Employee> employees = new List<Employee>();
+        private static readonly string connStr = "Server=10.56.8.36;Datebase=PEDB03;User Id=PE-03;Password=OPENDB_03";
+
+
+        public EmployeeRepo()
+        {
+            InitializeRepo();
+        }
+
+        public void InitializeRepo()
+        {
+            Employee employee;
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+
+                SqlCommand command = new SqlCommand("SELECT EmployeeId, Name, JobPosition, Mail, PhoneNumber FROM Employee", conn);
+
+                using(SqlDataReader reader = command.ExecuteReader())
+                {
+                    while(reader.Read())
+                    {
+                        employee = new Employee();
+                        employee.EmployeeId = int.Parse(reader["EmployeeId"].ToString());
+                        employee.Name = reader["Name"].ToString();
+                        employee.JobPosition = reader["JobPosition"].ToString();
+                        employee.Mail = reader["Mail"].ToString();
+                        employee.PhoneNumber = reader["PhoneNumber"].ToString();
+
+                        employees.Add(employee);
+                    }
+                }
+            }
+        }
+
+        public void AddEmployee(string name, string jobPosition, string mail, string phoneNumber)
+        {
+            Employee e = new Employee();
+            e.EmployeeId = employees.Count;
+            e.Name = name;
+            e.JobPosition = jobPosition;
+            e.Mail = mail;
+            e.PhoneNumber = phoneNumber;
+
+            employees.Add(e);
+
+            using(SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                SqlCommand command = new SqlCommand("INSERT INTO dbo.Employee(Name, JobPosition, Mail, PhoneNumber)" +
+                                                    "VALUES (@name, @ jobPosition, @mail, @phoneNumber)", conn);
+                command.Parameters.Add("@name", System.Data.SqlDbType.NVarChar).Value = e.Name;
+                command.Parameters.Add("@jobPosition", System.Data.SqlDbType.NVarChar).Value = e.JobPosition;
+                command.Parameters.Add("@mail", System.Data.SqlDbType.NVarChar).Value = e.Mail;
+                command.Parameters.Add("@phoneNumber", System.Data.SqlDbType.NVarChar).Value = e.PhoneNumber;
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void RemoveEmployee(Employee oldEmployee)
+        {
+            if(employees.Contains(oldEmployee))
+            {
+                employees.Remove(oldEmployee);
+
+                using(SqlConnection conn = new SqlConnection(connStr))
+                {
+                    conn.Open();
+
+                    SqlCommand command = new SqlCommand("DELETE FROM Employee WHERE EmployeeId = @employeeId;", conn);
+
+                    command.Parameters.Add("@employeeId", System.Data.SqlDbType.Int).Value = oldEmployee.EmployeeId;
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void UpdateEmployee(int employeeId, string name, string jobPosition, string mail, string phoneNumber)
+        {
+            if(employees.Exists(e => e.EmployeeId == employeeId))
+            {
+                Employee temp = employees.Find(e => e.EmployeeId == employeeId);
+                temp.Name = name;
+                temp.JobPosition = jobPosition;
+                temp.Mail = mail;
+                temp.PhoneNumber = phoneNumber;
+
+                using (SqlConnection conn = new SqlConnection(connStr))
+                {
+                    conn.Open();
+
+                    SqlCommand command = new SqlCommand("UPDATE Employee SET Name = @name, JobPosition = @jobPosition, Mail = @mail, PhoneNumber = @phoneNumber" +
+                                                        "WHERE EmployeeId = @employeeId", conn);
+
+                    command.Parameters.Add(@name, System.Data.SqlDbType.NVarChar).Value = name;
+                    command.Parameters.Add(@jobPosition, System.Data.SqlDbType.NVarChar).Value = jobPosition;
+                    command.Parameters.Add(@mail, System.Data.SqlDbType.NVarChar).Value = mail;
+                    command.Parameters.Add(@phoneNumber, System.Data.SqlDbType.NVarChar).Value = phoneNumber;
+
+                    command.ExecuteNonQuery();
+                }
+            }           
+        }
+
+        public List<Employee> GetAllEmployee()
+        {
+            return employees;
+        }
+
+        public Employee GetEmployeeById(int employeeId)
+        {
+            if(!employees.Exists(e => e.EmployeeId == employeeId))
+            {
+                return null;
+            }
+            return employees.Find(e => e.EmployeeId == employeeId);
+        }
+    }
+}
